2e7692a41d421f6b280904de78fa8f7024523c6d
hfkhrni <hesham.elfakharani@gmail.com>
2024-02-06 07:01:34 +0200

    feat: crawl single page

--- crawl.js
+++ crawl.js
@@ -1,5 +1,25 @@
 const { JSDOM } = require("jsdom");
 
+async function crawlPage(currURL) {
+  console.log(`crawling ${currURL}`);
+  try {
+    const res = await fetch(currURL);
+    if (!res.ok) {
+      console.log(`err with fetch with status: ${res.status} on url: ${currURL}`);
+      return;
+    }
+
+    const contentType = res.headers.get("content-type");
+    if (!contentType.includes("text/html")) {
+      console.log(`non html, content-type: ${contentType} on url: ${currURL}`);
+      return;
+    }
+    console.log(await res.text());
+  } catch (err) {
+    console.log(`err with fetch: ${err.message}`);
+  }
+}
+
 function getURLSfromHTML(htmlBody, baseURL) {
   const urls = [];
   const dom = new JSDOM(htmlBody);
@@ -41,4 +61,5 @@ function normalizeURL(urlString) {
 module.exports = {
   normalizeURL,
   getURLSfromHTML,
+  crawlPage,
 };
--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("hello");
+const { crawlPage } = require("./crawl");
+function main() {
+  if (process.argv.length < 3) {
+    console.log("no url provided");
+    process.exit(1);
+  }
+
+  if (process.argv.length > 3) {
+    console.log("too many argumments provided");
+    process.exit(1);
+  }
+  console.log(crawlPage(process.argv[2]));
+}
+main();
