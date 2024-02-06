bb27a39afedcd01885863d9a090c9a379f5d98e7
hfkhrni <hesham.elfakharani@gmail.com>
2024-02-06 07:59:50 +0200

    feat: print pages in desc sorted order

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { crawlPage } = require("./crawl");
+const { sortPages } = require("./report");
 async function main() {
   if (process.argv.length < 3) {
     console.log("no url provided");
@@ -11,8 +12,10 @@ async function main() {
   }
   const baseURL = process.argv[2];
   const pages = await crawlPage(baseURL, baseURL, {});
-  for (const page of Object.entries(pages)) {
-    console.log(page);
-  }
+  //   for (const page of Object.entries(pages)) {
+  //     console.log(page);
+  //   }
+
+  console.log(sortPages(pages));
 }
 main();
---
+++ report.js
@@ -0,0 +1,11 @@
+function sortPages(pages) {
+  pagesArr = Object.entries(pages);
+  pagesArr.sort((a, b) => {
+    return b[1] - a[1];
+  });
+  return pagesArr;
+}
+
+module.exports = {
+  sortPages,
+};
